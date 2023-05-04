336fa952ef8e32e664e0af45eb71f7e15f04ac47
Milan Suvajac <milansuvajac021@gmail.com>
2023-05-04 18:34:26 +0200

    feat: Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Home, Profile, Offers, SignIn, SignUp, ForgotPassword } from "./pages";
 
 function App() {
 	return (
 		<>
-			<div>Realtor App</div>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/proflie" element={<Profile />} />
+					<Route path="/offers" element={<Offers />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/forgot-password" element={<ForgotPassword />} />
+				</Routes>
+			</Router>
 		</>
 	);
 }
