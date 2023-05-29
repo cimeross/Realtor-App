a2d6f5dd96fdf9c327dd9d24d92af3385a541ff8
Milan Suvajac <milansuvajac021@gmail.com>
2023-05-29 20:31:52 +0200

    Created slider component and add it to the home page.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { db, doc } from "../firebase.config";
 import { getDoc } from "firebase/firestore";
-import Spinner from "./components/Spinner";
+import Spinner from "./Components/Spinner";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, {
 	EffectFade,
