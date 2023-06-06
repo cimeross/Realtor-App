f57d2f71cb1fdaba853c9b6eeb5b81efc9b2748f
Milan Suvajac <milansuvajac021@gmail.com>
2023-06-06 18:46:46 +0200

    update: Finished responivness for all media screens.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -38,10 +38,10 @@ const SignIn = () => {
 	}
 
 	return (
-		<section>
+		<section className="mx-auto">
 			<h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
 			<div className="flex justify-between flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
-				<div className="md:w-[67%] lg:w-[50%] ">
+				<div className="md:w-[67%] lg:w-[50%] mb-4">
 					<img
 						className="w-full rounded-xl"
 						src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"
